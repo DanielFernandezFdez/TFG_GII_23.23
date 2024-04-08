84851e7a37d99584cf01cacd71f2a48f0bb57f88
DanielFernandezFdez <dfff1002@alu.ubu.es>
2024-04-08 16:32:15 +0200

    Operaciones con usuarios

--- frontendAngular/src/app/administracion/pages/gestion-usuario/gestion-usuario.component.ts
+++ frontendAngular/src/app/administracion/pages/gestion-usuario/gestion-usuario.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import Swal from 'sweetalert2';
+import { RolesService } from '../../../services/roles.service';
+import { OverlayPanel } from 'primeng/overlaypanel';
 
 
 @Component({
@@ -10,11 +12,18 @@ import Swal from 'sweetalert2';
 })
 export class GestionUsuarioComponent implements OnInit {
   usuarios: any[] = [];
+  roles: any[] = [];
+  usuarioEditado: any = {};
+  usuarioNuevo: any = {};
 
-  constructor(private authService: AuthService) {}
+  @ViewChild('modificar') modificar?: OverlayPanel;
+  @ViewChild('nuevo') nuevo?: OverlayPanel;
+
+  constructor(private authService: AuthService, private rolesService:RolesService) {}
 
   ngOnInit(): void {
     this.cargarUsuarios();
+    this.cargarRoles();
   }
 
   cargarUsuarios(): void {
@@ -28,8 +37,90 @@ export class GestionUsuarioComponent implements OnInit {
     });
   }
 
-  editarUsuario(id: number): void {
-    console.log('Editar usuario', id);
+  cargarRoles(): void {
+    this.rolesService.obtenerRoles().subscribe({
+      next: (roles) => {
+        this.roles = roles;
+      },
+      error: (error) => {
+        console.error('Error al obtener los roles', error);
+      }
+    });
+  }
+
+  nuevoUsuario(event:MouseEvent): void {
+    
+    if(this.nuevo){
+      this.nuevo.toggle(event);
+    }
+    
+  }
+
+  guardarCambiosNuevo(): void {
+    this.authService.registrarUsuario(this.usuarioNuevo).subscribe({
+      next: () => {
+        Swal.fire({
+          toast:true,
+          position: 'top-end',
+          icon: 'success',
+          title: 'Usuario creado correctamente',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.cargarUsuarios();
+        if(this.nuevo){
+          this.nuevo.hide();
+          this.limpiarCampos();
+        }
+        
+      },
+      error: (error) => {
+        this.cargarUsuarios();
+        Swal.fire('Error', 'Ha ocurrido un error al crear el usuario', 'error');
+        this.limpiarCampos();
+      }
+    });
+  }
+
+  limpiarCampos() {
+    this.usuarioNuevo = {
+      nombre: '',
+      correo: '',
+      rol: ''
+    };
+  }
+
+  editarUsuario(id: number, event:MouseEvent): void {
+    const usuario = this.usuarios.find(u => u.id === id);
+    this.usuarioEditado = JSON.parse(JSON.stringify(usuario)); //Evita que se reflejen los cambios en la tabla
+
+    if(this.modificar){
+      this.modificar.toggle(event);
+    }
+    
+  }
+  guardarCambiosModificacion(): void {
+    this.authService.modificarUsuario(this.usuarioEditado.id, this.usuarioEditado).subscribe({
+      next: () => {
+        Swal.fire({
+          toast:true,
+          position: 'top-end',
+          icon: 'success',
+          title: 'Usuario actualizado correctamente',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.cargarUsuarios();
+        if(this.modificar){
+          this.modificar.hide();
+        }
+        
+      },
+      error: (error) => {
+        this.cargarUsuarios();
+        Swal.fire('Error', 'Ha ocurrido un error al actualizar el usuario', 'error');
+      }
+    });
   }
 
 
