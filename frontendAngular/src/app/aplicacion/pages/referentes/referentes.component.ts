0efd620ce95572c81b0206a11d0073b0d21ad86f
DanielFernandezFdez <dfff1002@alu.ubu.es>
2024-05-29 17:38:42 +0200

    CSS responsive y referentes

--- frontendAngular/src/app/aplicacion/pages/referentes/referentes.component.ts
+++ frontendAngular/src/app/aplicacion/pages/referentes/referentes.component.ts
@@ -5,18 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './referentes.component.html',
   styleUrl: './referentes.component.css'
 })
-export class ReferentesComponent {
-  investigaciones = [
-    {
-      titulo: "Ejemplo 1",
-      descripcion: "Descripción 1",
-      url: "https://ejemplo.com/investigacion1"
-    },
-    {
-      titulo: "Ejemplo 2",
-      descripcion: "Descripción 2",
-      url: "https://ejemplo.com/investigacion2"
-    }
- 
-  ];
-}
+export class ReferentesComponent {}
