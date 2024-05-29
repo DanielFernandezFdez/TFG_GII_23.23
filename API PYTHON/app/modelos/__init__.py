95a05d48c55fc72f4b8b97735d8c7d0f2f587b2e
DanielFernandezFdez <dfff1002@alu.ubu.es>
2024-05-29 17:05:06 +0200

    Corrección

--- API PYTHON/app/modelos/__init__.py
+++ API PYTHON/app/modelos/__init__.py
@@ -1,4 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-from .modelos import Libros, Libros_automaticos, fecha_modificacion, GestionEstimacion, Roles, Usuarios, Botones, Estimacion, EstadisticasPorMes,EstadisticasPorMesAuxiliar
+from .modelos import Libros, Libros_automaticos, fecha_modificacion, GestionEstimacion, Roles, Usuarios, Botones, Estimacion, EstadisticasPorMes,EstadisticasPorMesAuxiliar,Colaboradores
